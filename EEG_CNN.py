ec17324653db52adf17b6822a874ad4624ab92f6
Rohan Varma <rvarm1@gmail.com>
2018-03-08 18:32:22 -0800

    try this

--- EEG_CNN.py
+++ EEG_CNN.py
@@ -92,7 +92,7 @@ X_train, y_train, X_test, y_test = data_loader.load_all_data()
 X_train, y_train = X_train[0], y_train[0] # 238 * 22 * 1000 or something
 for epoch in range(10):
     # 10 epochs, 10 images
-    for i in range(X_train.shape[0]):
+    for i in range(20):
         image, label = X_train[i], y_train[i]
         if np.any(np.isnan(image)):
             print('skipping a nan entry')
